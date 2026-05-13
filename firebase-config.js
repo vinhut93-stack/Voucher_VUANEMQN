e860b80cdfc82cce31ef789967755f50ceeac3a1
PHAN ĐINH VI NHỰT <vinhut93@gmail.com>
2026-05-13 10:26:35 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,15 @@
  const firebaseConfig = { 
-    apiKey: "AIzaSyCFPWPMIHdZUJwpNJ75o1FK9AamecKSRCI", 
+    apiKey: "AIzaSyCSccrNwgBiVGfK5BWXelAdzHC5DZNm7lw", 
     authDomain: "vuanem-d66e5.firebaseapp.com",
-    projectId: "vuanem-d66e5"
+    projectId: "vuanem-d66e5",
+    storageBucket: "vuanem-d66e5.firebasestorage.app",
+    messagingSenderId: "430491577223",
+    appId: "1:430491577223:web:92be899aa6eae7c0950122"
 };
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+// Khởi tạo Firebase
+firebase.initializeApp(firebaseConfig);
 
+// Export global
 const auth = firebase.auth();
 const db = firebase.firestore();
